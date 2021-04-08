8bd5d368f393a27bd89ae96b9250dada75f241ba
PedrumK <pedrumkholghi@gmail.com>
2021-04-08 14:27:44 +0100

    Add database connection

--- retrv/backend/server.js
+++ retrv/backend/server.js
@@ -1,9 +1,26 @@
+require('dotenv').config({ path: '.env '})
 const { PORT } = require('./config');
 
-// import app
+// Import app
 const app = require('./app');
 
-// start the server
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// Start the Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Database Connection
+mongoose.connect(process.env.DATABASE,
+  {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+  }
+);
+
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+  console.error(`Database Connection Error -> ${err.message}`);
 });
